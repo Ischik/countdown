6720914f977690e4a0c6b38e16bf3eee47b2f2ec
Ischik <98402323+Ischik@users.noreply.github.com>
2022-01-25 16:05:29 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+for (let index = 0; index <= 9; index++) {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    basic.showNumber(10 - index)
+}
+music.playTone(392, music.beat(BeatFraction.Whole))
+basic.showString("GO")
+basic.forever(function () {
+	
+})
